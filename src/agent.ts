fad911307f7750fb899226ca320e1626356cfdc0
btree Orion <mountainorion@proton.me>
2025-10-18 23:30:39 -0600

    moved twitter plugin from seperate file

--- src/agent.ts
+++ src/agent.ts
@@ -1,8 +1,6 @@
-import { GameAgent, GameWorker, LLMModel } from "@virtuals-protocol/game";
-import TwitterPlugin from "@virtuals-protocol/game-twitter-plugin";
-import { TwitterApi } from "@virtuals-protocol/game-twitter-node";
-// import { TwitterWorker } from "./twitterWorker";
+import { GameAgent } from "@virtuals-protocol/game";
 import dotenv from "dotenv";
+import { TwitterWorker } from "./twitterWorker";
 import AlchemyPlugin from "./alchemyPlugin";
 
 dotenv.config();
@@ -25,51 +23,13 @@ const alchemyPlugin = new AlchemyPlugin({
     },
 });
 
-const gameTwitterClient = new TwitterApi({
-  gameTwitterAccessToken: process.env.GAME_TWITTER_ACCESS_TOKEN,
-});
-
-// const nativeTwitterClient = new TwitterApi({
-//   appKey: "xxxxxxx",
-//   appSecret: "xxxxxxx",
-//   accessToken: "xxxxxxx",
-//   accessSecret: "xxxxxxxxx",
-// });
-
-// Create a worker with the functions
-const twitterPlugin = new TwitterPlugin({
-  id: "twitter_worker",
-  name: "Twitter Worker",
-  description:
-    "A worker that will execute tasks within the Twitter Social Platforms. It is capable of posting, reply, quote and like tweets.",
-  twitterClient: gameTwitterClient,
-});
-
-
 export const activity_agent = new GameAgent(process.env.VIRTUALS_API_TOKEN, {
     name: "XAO Arbitration Agent",
     goal: "find tweets from @btreeOrion or tweets that mention @btreeOrion",
     description: "A bot that can post tweets, reply to tweets, and like tweets",
     workers: [
         alchemyPlugin.getWorker(),
-        // TwitterWorker
-        new GameWorker({
-            id: "twitter_worker",
-            name: "Twitter Worker",
-            description: "Twitter integration worker",
-            functions: [
-                twitterPlugin.searchTweetsFunction,
-                //twitterPlugin.replyTweetFunction,
-                //twitterPlugin.postTweetFunction,
-            ],
-            getEnvironment: async () => {
-                return {
-                ...(await twitterPlugin.getMetrics()),
-                username: "virtualsprotocol",
-                token_price: "$100.00",
-                };
-            },
-        }),
+        TwitterWorker        
     ]
 });
 
